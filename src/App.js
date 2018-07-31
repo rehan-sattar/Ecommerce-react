4c5a04c958bcac62d73ff883eb53787e2ebd23ca
Rehan-Sattar <rehansattar117@gmail.com>
2018-07-31 10:44:30 -0700

    push back to orignin

--- src/App.js
+++ src/App.js
@@ -1,42 +1,14 @@
 import React, { Component } from 'react';
-import { Row, Col, Card, Icon, } from "antd";
-import { Layout } from "antd";
 import { BrowserRouter , Route, Link } from "react-router-dom";
-// custom files
-import WrappedNormalLoginForm from "./components/Forms/Login";
-// import Header from "./components/Headers/loggedInHeader"
-import Dashboard from "./components/PublicProducts/publicProducts";
 import './App.css';
-import WrappedRegistrationForm from './components/Forms/signUp';
-const { Header  } = Layout;
 
-const styles = {
-  login_card: {
-    marginTop: '5em',
-    textAlign: 'center'
-  },
-  header : {
-    color : "#ffff"
-  }
-}
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <div className="App-overlay">
-            <BrowserRouter>
-                <div>
-                <Header style={styles.header}>
-                    <Link to="/"> Home  </Link>
-                    <Link to="/create-account"> Create Account  </Link> 
-                    <Link to="/dashboard"> All Products  </Link> 
-                 </Header>
-                  <Route path="/" exact component={WrappedNormalLoginForm} />
-                  <Route path="/create-account" component={WrappedRegistrationForm} />
-                  <Route path="/dashboard" component={Dashboard} />
-                </div>
-            </BrowserRouter>
+          <div className="App-overlay">
+            hello olx
         </div>
       </div>
     );
---
+++ src/components/notFound.js
@@ -0,0 +1,9 @@
+import  React from 'react';
+
+const notFound = () => (
+    <div>
+        <h1>PAGE NOT FOUND!!</h1>
+    </div>
+);
+
+export default notFound;
