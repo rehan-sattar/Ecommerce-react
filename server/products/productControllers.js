e1079a7dc091acb55b251727f2dd8997ab3a81d8
Rehan-Sattar <rehansattar117@gmail.com>
2018-08-17 01:35:31 +0500

    products

--- server/products/productControllers.js
+++ server/products/productControllers.js
@@ -1,10 +1,11 @@
 const mongoose = require('mongoose');
 const Product = require('./modal');
-
+/////////////// addProductAttempt ////////////////////////
 const addProductAttempt = (request, response) => {
     const product = new Product({
         title: request.body.title,
         name: request.body.name,
+        cataqgory: request.body.catagory,
         description: request.body.description,
         model: request.body.model,
         year: request.body.year,
@@ -13,20 +14,26 @@ const addProductAttempt = (request, response) => {
     product
         .save()
         .then(res => {
-            response.send({ status: true, res })
+            response.send({
+                status: true,
+                pdoductData: res,
+                message: 'Product has been added!'
+            })
         })
         .catch(err => console.log(err));
 };
 
+///////////getAllProductAttempt//////////////////
 const getAllProductAttempt = (request, response) => {
-    Product.find().select('title name description price')
+    Product.find().select('title name catagory description price')
         .then(docs => {
             if (docs.length >= 0) {
                 const desiredResponse = {
                     totalProducts: docs.length,
                     products: docs.map(doc => ({
-                        name : doc.name,
-                        title : doc.title,
+                        name: doc.name,
+                        title: doc.title,
+                        catagory: doc.catagory,
                         description: doc.description,
                         price: doc.price,
                         request: {
@@ -43,21 +50,36 @@ const getAllProductAttempt = (request, response) => {
         })
         .catch(err => respons.send({ status: 500, err }))
 }
+
+///////////////// getProductAttempt   /////////////////////////
+
 const getProductAttempt = (request, response) => {
     const id = request.params.productId;
     Product.findById(id)
+        .select('name title description price')
         .then(doc => {
             console.log(`Response from :/Product: ${doc}`);
             if (doc) {
-                response.send({ status: 200, doc });
+                const doc = {
+                    name: doc.name,
+                    title: doc.title,
+                    catagory: doc.catagory,
+                    description: doc.description,
+                    price: doc.price,
+                    request: {
+                        type: 'GET',
+                        url: `localhost:8080/dashboard/products/${doc._id}`
+                    }
+                }
+                response.send({ status: 200, product });
             } else {
                 response.send({ status: 404, message: 'No valid entry found for provided productID' });
             }
         })
         .catch(err => response.send({ status: 500, err }))
-
 }
 
+///////////// deleteProduct ///////////////
 const deleteProduct = (request, response) => {
     const _id = request.params.id;
     Product.remove({ _id })
@@ -66,12 +88,85 @@ const deleteProduct = (request, response) => {
         })
         .catch(err => response.send({ status: 500, message: err.message }))
 }
+//////////////// SearchProductAttempt /////////////
 const searchProductAttempt = (req, res) => {
-    res.send('you are in searchProductAttempt');
+    const nameOfProduct = req.params.name
+    Product.find({
+        'name': `${nameOfProduct}`
+    })
+        .select('name title description price')
+        .then(doc => {
+            if (doc.length > 0) {
+                productArray = [];
+                doc.map(document => {
+                    let product = {
+                        name: document.name,
+                        title: document.title,
+                        catagory: document.catagory,
+                        description: document.description,
+                        price: document.price,
+                        request: {
+                            type: 'GET',
+                            url: `localhost:8080/dashboard/products/searchProduct/` + nameOfProduct
+                        }
+                    };
+                    productArray.push(product);
+                })
+
+                res.send({
+                    status: true,
+                    products: productArray
+                });
+            } else {
+                res.send({
+                    status: 404,
+                    message: 'No record Found for this name'
+                });
+            }
+        })
+        .catch(err => {
+            console.log(`Error: ${err}`);
+        })
 };
 
 const searchViaCatagoryAttempt = (req, res) => {
-    res.send('You are in searchViaCatagoryAttempt');
+    const catagoryName = req.params.name
+    Product.find({
+        'name': `${catagoryName}`
+    })
+        .select('name title description price')
+        .then(doc => {
+            if (doc.length > 0) {
+                productArray = [];
+                doc.map(document => {
+                    let product = {
+                        name: document.name,
+                        title: document.title,
+                        catagory: document.catagory,
+                        description: document.description,
+                        price: document.price,
+                        request: {
+                            type: 'GET',
+                            url: `localhost:8080/dashboard/products/searchProduct/` + catagoryName
+                        }
+                    };
+                    productArray.push(product);
+                })
+
+                res.send({
+                    status: true,
+                    products: productArray
+                });
+            } else {
+                res.send({
+                    status: 404,
+                    message: 'No record Found for this name'
+                });
+            }
+        })
+        .catch(err => {
+            console.log(`Error: ${err}`);
+        })
 };
 
 module.exports = {
