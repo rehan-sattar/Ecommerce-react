c1c4bd246dc0cfc5eaf7fc189ab04779191a8ad8
Rehan-Sattar <rehansattar117@gmail.com>
2018-08-25 00:47:19 -0700

    Custome Middlewares

--- server/middleware/auth-access-middleware.js
+++ server/middleware/auth-access-middleware.js
@@ -1,12 +1,15 @@
 const jwt = require('jsonwebtoken');
 module.exports = (req, res, next) => {
-   try {
-        const decodedData = jwt.verify(req.body.token , 'olx-pakistan' );
+    try {
+        const token = req.headers.authorization.split(" ")[1];
+        console.log(token);
+        const decodedData = jwt.verify(token, 'olx-pakistan');
+        console.log("Decoded Data",decodedData);
         req.userData = decodedData
         next();
-   } catch( err ) {
+    } catch (err) {
         return res.status(401).send({
             message: 'Access denied!'
         })
-   }
+    };
 };
