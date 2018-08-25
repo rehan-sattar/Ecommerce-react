f6c903a39a8a427385debb4baea5b79cb76a61a5
Rehan-Sattar <rehansattar117@gmail.com>
2018-08-25 00:47:42 -0700

    Product Modal

--- server/products/modal.js
+++ server/products/modal.js
@@ -3,9 +3,11 @@ const productScehma = mongoose.Schema({
     title: { type: String, required: true },
     name: { type: String, required: true },
     description: { type: String, required: true },
-    // year: { type: Number, required: true },
+    year: { type: Number, required: true },
     catagory: { type: String, require: true },
-    price: { type: Number, required: true }
+    price: { type: Number, required: true },
+    sellerPhone : { type : String, required: true },
+    sellerLocation : { type: String, required: true}
 });
 
-module.exports = mongoose.model('Product', productScehma);
+module.exports = mongoose.model('Products', productScehma);
