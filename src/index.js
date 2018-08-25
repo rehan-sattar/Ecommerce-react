8dc84bf5649a3e7ac526befbb6efb0d45832baa7
Rehan-Sattar <rehansattar117@gmail.com>
2018-08-25 00:46:13 -0700

    Entry point to React

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import App from './App';
 import store from "./store/Store";
 import { Provider } from "react-redux";
 import registerServiceWorker from './registerServiceWorker';
-import { _firebase } from "./firebase/firebase";
+
 
 ReactDOM.render(
 <Provider store={store}>
@@ -13,12 +13,3 @@ ReactDOM.render(
 </Provider>
     , document.getElementById('root'));
 registerServiceWorker();
-
-
-_firebase.auth().onAuthStateChanged( user => {
-    if( user ) {
-        console.log('user logged in');
-    } else {
-        console.log('user Logged out');
-    }
-})
