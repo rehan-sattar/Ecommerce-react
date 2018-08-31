63f8fa4aaf1bbb912b74240b7e58db1a9b9018b5
Rehan-Sattar <rehansattar117@gmail.com>
2018-08-31 00:23:14 -0700

    Public Products

--- src/components/PublicProducts/publicProducts.jsx
+++ src/components/PublicProducts/publicProducts.jsx
@@ -1,42 +1,56 @@
 import React from 'react';
 import ProductCard from "./ProductCard";
+import { connect } from "react-redux";
+import { bindActionCreators } from "redux";
+import { getAllProductAttempt } from "../../store/Actions/productActions";
 
-class PublicProducts extends React.Component{
+
+class PublicProducts extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      products: []
+    };
+  };
+  componentDidMount() {
+    console.log("Call herer")
+    this.props.downloadAllProducts()
+  }
+  componentWillReceiveProps(props) {
+    console.log('Component Will Reciece Props : ', props.allProducts.allProducts.products);
+    this.setState({
+      products: props.allProducts.allProducts.products
+    })
+  }
   render() {
-    const listOfProducts = [
-      {
-        id : 1,
-        name : 'P1'
-      },
-      {
-        id : 2,
-        name : 'P2'
-      },
-      {
-        id : 3,
-        name : 'P3'
-      },
-      {
-        id : 5,
-        name : 'P5'
-      },
-      {
-        id : 6,
-        name : 'P6'
-      },
-      {
-        id : 7,
-        name : 'P7'
-      },
-    ]
-    return(
+    return (
       <div className="container mt-5">
         <h1 className="text-center">All Adds</h1>
-          <div className="row">
-              {listOfProducts.map( product => <ProductCard productItem={product} />)}
-          </div>
+        {
+            console.log('public products: ', this.props.allProducts)}
+        <div className="row">
+          {
+            this.state.products.length > 0 ? this.state.products.map((product) => (
+              <div key={product._id} className="col-md-4 col-lg-4 col-sm-12">
+                <ProductCard productItem={product} />
+              </div>)
+            ) : <p>No Records Till Now</p>
+
+          }
+        </div>
       </div>
     )
   }
 }
-export default PublicProducts;
+
+const mapStateToProps = (state) => {
+  return {
+    allProducts: state.productReducer.allProducts
+  };
+}
+const mapDispatchToProps = (dispatch) => {
+  return bindActionCreators({
+    downloadAllProducts: () => getAllProductAttempt()
+  }, dispatch)
+}
+export default connect(mapStateToProps, mapDispatchToProps)(PublicProducts);
