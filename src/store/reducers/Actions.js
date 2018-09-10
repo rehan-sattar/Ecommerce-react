210ffd0c8977c9994dd3cf2cba12cb549acce19c
Rehan-Sattar <rehansattar117@gmail.com>
2018-09-10 02:39:05 -0700

    All Actions | action.type_OBJECT

--- src/store/reducers/Actions.js
+++ src/store/reducers/Actions.js
@@ -3,7 +3,11 @@ export const UserActions = {
     singInSuccess: 'SIGN_IN_SUCCESS',
     signInError: 'SIGN_IN_ERROR',
     signUpSuccess: 'SIGN_UP_SUCCESS',
-    signUpError: 'SIGN_UP_ERROR'
+    signUpError: 'SIGN_UP_ERROR',
+    addToFavoritSuccess : 'ADD_TO_FAV_SUCCESS',
+    addToFavoritError : 'ADD_TO_FAV_ERROR',
+    getAllFavProductsSuccess : 'GET_ALL_FAV_PRODUCTS_SUCCESS'
+    
 };
 
 export const ProductActions = {
@@ -13,4 +17,4 @@ export const ProductActions = {
     searchProductByCatagoryError : 'CATAGORY_SEARCH_ERROR',
     getAllProductsSuccess : 'GET_All_PRODUCTS_SUCCESS',
     getAllProductsError : 'GET_All_PRODUCTS_ERROR',
-}
+};
