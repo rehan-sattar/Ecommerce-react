1dab16504d63bf6ab7cac25694ebc53100db5d80
Rehan-Sattar <rehansattar117@gmail.com>
2018-09-10 02:39:54 -0700

    Entry point to Application

--- src/App.js
+++ src/App.js
@@ -1,20 +1,34 @@
 import React, { Component } from 'react';
-import { Router} from "react-router-dom";
+import { Router } from "react-router-dom";
+import { connect } from "react-redux";
 import { createBrowserHistory } from "history";
 import AppRouter from "./routes/AppRouter";
 import './App.css';
+import HeaderLoggedOut from "./components/Headers/loggedOutHeader";
+import HeaderLoggedIn from "./components/Headers/loggedInHeader";
 const history = createBrowserHistory();
 
 class App extends Component {
+  componentWillReceiveProps(props) {
+    console.log('isUserAuthenticated: ' , props.isUserAuthenticated);
+  }
   render() {
     return (
       <div className="App">
         <Router history={history}>
+          <div>
+            <HeaderLoggedOut />
             <AppRouter />
+          </div>
         </Router>
       </div>
     );
   }
 }
 
-export default App;
+const mapStateToProps = (state) => {
+  return {
+    isUserAuthenticated : state.userReducer.isAuthenticated
+  }
+}
+export default connect(mapStateToProps,undefined)(App);
