92c5ac5fedad8bda399a356afe07c18cd8ede9bb
Rehan-Sattar <rehansattar117@gmail.com>
2018-09-10 02:39:28 -0700

    All Routes

--- src/routes/AppRouter.js
+++ src/routes/AppRouter.js
@@ -4,28 +4,24 @@ import Home from "../components/home/Home";
 import signUp from "../components/Forms/signUp";
 import LoginForm from "../components/Forms/Login";
 import notFound from "../components/notFound"
-import HeaderLoggedOut from "../components/Headers/loggedOutHeader";
-import HeaderLoggedIn from "../components/Headers/loggedInHeader";
 import AddProductForm from "../components/userDashboard/addProduct";
 import SearchProduct from "../components/userDashboard/searchProduct";
 import userDashboard from "../components/userDashboard/dashboard";
-import PublicProducts from "../components/PublicProducts/publicProducts"
-
+import PublicProducts from "../components/PublicProducts/publicProducts";
+import ProductDetailShowcase from "../components/ProductDetailShowcase";
+import FavoritProductContainer from "../components/userDashboard/favProducts";
 const AppRouter = () => (
   <main>
-    
-    <HeaderLoggedOut />
-
-    {/* <HeaderLoggedIn /> */}
     <Switch>
       <Route exact path='/' component={Home} />
       <Route path='/user/signIn' component={LoginForm} />
       <Route path='/user/signUp' component={signUp} />
       <Route path='/user/publicProducts' component={PublicProducts} />
-      <Route path="/user/publicProducts/:product" />
+      <Route path="/user/publicProducts/:id" exact component={ProductDetailShowcase} />
       <Route path='/dashboard' exact component={userDashboard} />
       <Route path="/dashboard/addProduct" component={AddProductForm} />
       <Route path="/dashboard/searchProduct" component={SearchProduct} />
+      <Route path="/dashboard/favoritProducts" component={FavoritProductContainer} />
       <Route component={notFound} />
     </Switch>
   </main>
