a240e1c969bea800b0bd139750ffdff9a996765d
Rehan-Sattar <rehansattar117@gmail.com>
2018-09-10 02:41:26 -0700

    Home Container

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,14 +1,10 @@
 import React from "react";
 import LoginForm from "../Forms/Login"
-import signUp from "../Forms/signUp";
-import loggedInHeader from "../Headers/loggedInHeader";
-import PublicProducts from "../PublicProducts/publicProducts";
 
 class Home extends React.Component {
   render() {
     return (
       <div>
-        {/* <loggedInHeader /> */}
         <LoginForm />
       </div>
     );
