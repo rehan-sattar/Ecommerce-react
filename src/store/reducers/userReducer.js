11cc33e7a02a393d412c216901d8533fe7bf03a1
Rehan-Sattar <rehansattar117@gmail.com>
2018-09-10 02:38:32 -0700

    user Logout Reducer

--- src/store/reducers/userReducer.js
+++ src/store/reducers/userReducer.js
@@ -1,28 +1,42 @@
 import { UserActions } from "./Actions";
+import swal from "sweetalert";
 const defaultState = {
-  users : null
+  users: null,
+  isAuthenticated: true,
+  AddedFavProductFlag: false,
+  allFavoritProducts: []
 };
-const userReducer = ( state = defaultState , action) => {
-  const newState = { ...state};
-    switch(action.type) {
-      case UserActions.singInSuccess:
-        newState.users = action.payload 
-        break;
-      case UserActions.signInError: 
-        break;
-      case UserActions.signUpSuccess:
-        newState.users = action.payload  
-        break;
-      case UserActions.signUpError:
-        break;
-      case 'GET_USER_LOGOUT':
-        console.log('GET_USER_LOGOUT HITTED!');
-        return {
-          user : null
-        }
-        break;
-    }
-    return newState;
+const userReducer = (state = defaultState, action) => {
+  const newState = { ...state };
+  switch (action.type) {
+    case UserActions.singInSuccess:
+      newState.users = action.payload;
+      newState.isAuthenticated = true
+      break;
+    case UserActions.signInError:
+      break;
+    case UserActions.signUpSuccess:
+      newState.users = action.payload;
+      newState.isAuthenticated = true;
+      break;
+    case UserActions.signUpError:
+      break;
+    case UserActions.addToFavoritSuccess:
+      swal("Wohamiiii!!", "This is your favorit one :)", "success");
+      newState.AddedFavProductFlag = true
+      break;
+    case UserActions.addToFavoritError:
+      break;
+    case UserActions.getAllFavProductsSuccess:
+      console.log('Response of all favorit products: ', action.payload);
+      newState.allFavoritProducts = action.payload;
+      break;
+    case 'GET_USER_LOGOUT':
+      newState.isAuthenticated = false;
+      localStorage.removeItem('userId');
+      break;
+  }
+  return newState;
 }
 
 export {
