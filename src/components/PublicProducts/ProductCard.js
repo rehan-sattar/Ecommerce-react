f842d0b49113a8a5c8a20375cb0fea4ce52532dd
Rehan-Sattar <rehansattar117@gmail.com>
2018-09-10 02:34:21 -0700

    Publically Available Containers

--- src/components/PublicProducts/ProductCard.js
+++ src/components/PublicProducts/ProductCard.js
@@ -1,18 +1,16 @@
 import React from 'react';
+import { Link } from "react-router-dom";
 
 class ProductCard extends React.Component {
   render() {
     return (
       <div>
         <div className="card my-3">
-          <img className="card-img-top" src="data:image/svg+xml;charset=UTF-8,%3Csvg%20width%3D%22286%22%20height%3D%22180%22%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%20viewBox%3D%220%200%20286%20180%22%20preserveAspectRatio%3D%22none%22%3E%3Cdefs%3E%3Cstyle%20type%3D%22text%2Fcss%22%3E%23holder_164f6582b11%20text%20%7B%20fill%3Argba(255%2C255%2C255%2C.75)%3Bfont-weight%3Anormal%3Bfont-family%3AHelvetica%2C%20monospace%3Bfont-size%3A14pt%20%7D%20%3C%2Fstyle%3E%3C%2Fdefs%3E%3Cg%20id%3D%22holder_164f6582b11%22%3E%3Crect%20width%3D%22286%22%20height%3D%22180%22%20fill%3D%22%23777%22%3E%3C%2Frect%3E%3Cg%3E%3Ctext%20x%3D%22107.1953125%22%20y%3D%2296.3%22%3E286x180%3C%2Ftext%3E%3C%2Fg%3E%3C%2Fg%3E%3C%2Fsvg%3E" alt="Card image cap" />
+          <img className="card-img-top img-fluid" src={`${this.props.productItem.imagebaseLink}`} alt="Card image cap" />
           <div className="card-body" >
             <h3>
               <u>{this.props.productItem.title}</u>
             </h3>
-            <p className="leadd">
-              {this.props.productItem.description}
-            </p>
             <p>
               <small>
                 Catagory :
@@ -25,9 +23,20 @@ class ProductCard extends React.Component {
               </span>
             </h5>
           </div>
-          <div className="card-footer">
-            <button className="btn btn-outline-info mx-2"> <i className="fa fa-star"></i> add to fav</button>
-            <button className="btn btn-outline-primary"> <i className="fa fa-plus"></i> view details</button>
+          <div className="card-footer text-center">
+
+            {
+              this.props.favFunct ? (
+              <button className="btn btn-outline-primary btn-block btn-lg mx-1" onClick={ () => this.props.addTofavoritHandle(this.props.productItem._id)}>
+                <i className="fa fa-star"></i>
+                  Add to fav
+              </button>
+              ) : null
+            }
+            <button className="btn btn-warning btn-block btn-lg mx-1">
+              <i className="fa fa-plus mx-2 text-white"></i>
+              <Link to={`/user/publicProducts/${this.props.productItem._id}`} className="text-white" >view details </Link>
+            </button>
           </div>
         </div>
       </div>
@@ -36,25 +45,5 @@ class ProductCard extends React.Component {
 };
 
 
+
 export default ProductCard;
-
-// title: { type: String, required: true },
-// name: { type: String, required: true },
-// description: { type: String, required: true },
-// year: { type: Number, required: true },
-// catagory: { type: String, require: true },
-// price: { type: Number, required: true },
-// sellerPhone : { type : String, required: true },
-// sellerLocation : { type: String, required: true}
-
-{/*
-
-  <div class="card"">
-  <img class="card-img-top" src="..." alt="Card image cap">
-  <div class="card-body">
-    <h5 class="card-title">Card title</h5>
-    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-    <a href="#" class="btn btn-primary">Go somewhere</a>
-  </div>
-</div>
-*/}
--- src/components/PublicProducts/publicProducts.jsx
+++ src/components/PublicProducts/publicProducts.jsx
@@ -3,39 +3,60 @@ import ProductCard from "./ProductCard";
 import { connect } from "react-redux";
 import { bindActionCreators } from "redux";
 import { getAllProductAttempt } from "../../store/Actions/productActions";
+import { EEXIST } from 'constants';
 
 
 class PublicProducts extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      products: []
+      products: [],
+      productsForSearching: []
     };
+    this.setCatagory = this.setCatagory.bind(this);
   };
+
   componentDidMount() {
-    console.log("Call herer")
     this.props.downloadAllProducts()
   }
   componentWillReceiveProps(props) {
-    console.log('Component Will Reciece Props : ', props.allProducts.allProducts.products);
     this.setState({
-      products: props.allProducts.allProducts.products
+      products: props.allProducts.allProducts.products,
+      productsForSearching: props.allProducts.allProducts.products
+    })
+  }
+  setCatagory(event) {
+    this.setState({
+      products: this.state.productsForSearching.filter(product => product.catagory === event.target.value)
     })
   }
   render() {
     return (
       <div className="container mt-5">
         <h1 className="text-center">All Adds</h1>
-        {
-            console.log('public products: ', this.props.allProducts)}
-        <div className="row">
+        <h5>Search products by catagory: </h5>
+        <select
+          onChange={this.setCatagory}
+          className="form-control" required>
+          <option value="select">Select Catagory</option>
+          <option value="propertyForSale">Property for Sale</option>
+          <option value="propertyForRent">Property For Rent</option>
+          <option value="bikes">Bikes</option>
+          <option value="cars">Cars</option>
+          <option value="electrnoicsAndHomeApp">Electronic & Home Appliances</option>
+          <option value="mobiles">Mobiles</option>
+          <option value="jobs">jobs</option>
+          <option value="services">Services</option>
+          <option value="furiture">Furniture</option>
+        </select>
+        <hr />
+        <div className="row mt-3">
           {
             this.state.products.length > 0 ? this.state.products.map((product) => (
               <div key={product._id} className="col-md-4 col-lg-4 col-sm-12">
-                <ProductCard productItem={product} />
+                <ProductCard favFunct={false} productItem={product} />
               </div>)
-            ) : <p>No Records Till Now</p>
-
+            ) : <p className="text-success text-center lead my-5">No records till now...</p>
           }
         </div>
       </div>
